a29b67ff29d3a216b55589294b204b96c02d4590
Josephine Joseph <jubyandfriends@gmail.com>
2022-01-03 13:24:03 -0600

    milestone-2-ui

--- salon-api/src/main/java/com/salon/learn/repository/SalonServiceDetailRepository.java
+++ salon-api/src/main/java/com/salon/learn/repository/SalonServiceDetailRepository.java
@@ -1,5 +1,7 @@
 package com.salon.learn.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,12 @@ import com.salon.learn.model.SalonServiceDetail;
 @Repository
 public interface SalonServiceDetailRepository extends  JpaRepository<SalonServiceDetail,Long>{
 
-}
+	
+	List<SalonServiceDetail> findAll();
+	
+	}
+	
+	
+	
+	
+
